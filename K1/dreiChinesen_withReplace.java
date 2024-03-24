fc9705429e61d8f973df66ab068a8dde2e7f49ab
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2024-03-24 15:34:23 +0100

    drei chinesen mit replace

---
+++ K1/dreiChinesen_withReplace.java
@@ -0,0 +1,41 @@
+public class drei_chinesen
+{
+    public drei_chinesen()
+    {
+        String DC = "Drei Chinesen mit dem Kontrabass saßen auf der Straße und erzählten sich was, da kam die Polizei, ja was ist den das? Drei Chinesen mit dem Kontrabass.";
+        DC = isDoubled(DC);
+        String[] Vokals = {"a","e","i","o","u","au"};
+        
+        for(String Vokal:Vokals)
+        {
+            System.out.println(replaceMultipleVocal(DC, Vokal));
+        }
+    }
+
+    public String replaceMultipleVocal(String str, String reStr)
+    {
+        return str.replaceAll("[aeiou]", reStr);
+    }
+
+    public boolean isVocal(char ch)
+    {
+        return "AEIOUÄÖÜaeiouäöü".indexOf(ch) != -1;
+    }
+
+    public String isDoubled(String str) 
+    {
+        char[] charArray = str.toCharArray();
+        char previousChar = charArray[0];
+        StringBuilder result = new StringBuilder();
+        for (char ch: charArray) 
+        {
+            if (isVocal(previousChar) && isVocal(ch))
+            {
+                continue;
+            }
+            result.append(ch);
+            previousChar = ch;
+        }
+        return result.toString();
+    }
+}
