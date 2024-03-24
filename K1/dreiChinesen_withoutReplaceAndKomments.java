74b34022067900603e15e01846c785d221728d12
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2024-03-24 16:00:49 +0100

    without replace

---
+++ K1/dreiChinesen_withoutReplaceAndKomments.java
@@ -0,0 +1,54 @@
+public class drei_chinesen
+{
+    public drei_chinesen()
+    {
+        String DC = "Drei Chinesen mit dem Kontrabass saßen auf der Straße und erzählten sich was, da kam die Polizei, ja was ist den das? Drei Chinesen mit dem Kontrabass.";
+        DC = isDoubled(DC);
+        String[] Vokals = {"a","e","i","o","u","au"};
+        
+        for(String Vokal:Vokals)
+        {
+            System.out.println(replaceMultipleVocal(DC, Vokal));
+        }
+    }
+
+    public String replaceMultipleVocal(String str, String reStr)
+    {
+        char[] charArray = str.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (char ch: charArray) 
+        {
+            if (isVocal(ch))
+            {
+                result.append(reStr);
+            }
+            else
+            {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+
+    public boolean isVocal(char ch)
+    {
+        return "AEIOUÄÖÜaeiouäöü".indexOf(ch) != -1;
+    }
+
+    public String isDoubled(String str) 
+    {
+        char[] charArray = str.toCharArray();
+        char previousChar = charArray[0];
+        StringBuilder result = new StringBuilder();
+        for (char ch: charArray) 
+        {
+            if (isVocal(previousChar) && isVocal(ch))
+            {
+                continue;
+            }
+            result.append(ch);
+            previousChar = ch;
+        }
+        return result.toString();
+    }
+}
