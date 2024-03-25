e96f4fbb1c7872616ffceac2349742f0b9508234
Benjamin <103788939+justforaprogramm@users.noreply.github.com>
2024-03-25 01:12:58 +0100

    Update dreiChinesen_withoutReplaceAndKomments.java

--- K1/dreiChinesen_withoutReplaceAndKomments.java
+++ K1/dreiChinesen_withoutReplaceAndKomments.java
@@ -3,22 +3,23 @@ public class drei_chinesen
     public drei_chinesen()
     {
         String DC = "Drei Chinesen mit dem Kontrabass saßen auf der Straße und erzählten sich was, da kam die Polizei, ja was ist den das? Drei Chinesen mit dem Kontrabass.";
-        DC = isDoubled(DC);
         String[] Vokals = {"a","e","i","o","u","au"};
         
+        StringBuilder result = new StringBuilder();
         for(String Vokal:Vokals)
         {
-            System.out.println(replaceMultipleVocal(DC, Vokal));
+            result.append(replaceMultipleVocal(isDoubled(DC), Vokal) + "\n");
         }
+        System.out.print(result.toString());
     }
 
-    public String replaceMultipleVocal(String str, String reStr)
+    private String replaceMultipleVocal(String str, String reStr)
     {
         char[] charArray = str.toCharArray();
         StringBuilder result = new StringBuilder();
-        for (char ch: charArray) 
+        for (char ch:charArray) 
         {
-            if (isVocal(ch))
+            if ("AEIOUÄÖÜaeiouäöü".indexOf(ch) != -1)
             {
                 result.append(reStr);
             }
@@ -30,24 +31,18 @@ public class drei_chinesen
         return result.toString();
     }
 
-    public boolean isVocal(char ch)
-    {
-        return "AEIOUÄÖÜaeiouäöü".indexOf(ch) != -1;
-    }
-
-    public String isDoubled(String str) 
+    private String isDoubled(String str) 
     {
         char[] charArray = str.toCharArray();
-        char previousChar = charArray[0];
+        char prevChar = charArray[0];
         StringBuilder result = new StringBuilder();
-        for (char ch: charArray) 
+        for (char ch:charArray) 
         {
-            if (isVocal(previousChar) && isVocal(ch))
+            if(!("AEIOUÄÖÜaeiouäöü".indexOf(prevChar) != -1 & "AEIOUÄÖÜaeiouäöü".indexOf(ch) != -1))
             {
-                continue;
+                result.append(ch);
             }
-            result.append(ch);
-            previousChar = ch;
+            prevChar = ch;
         }
         return result.toString();
     }
